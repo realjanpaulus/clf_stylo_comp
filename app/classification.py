6f9193bd61fa4021ab48ac9eb419470f6fd912b4
Jan Paulus <janpaulus95@web.de>
2020-01-05 15:59:34 +0100

    add: more classifier (LR, LSVM, MNB) and tuning

--- app/classification.py
+++ app/classification.py
@@ -2,18 +2,19 @@ import argparse
 from collections import defaultdict
 from datetime import datetime
 import logging
-import matplotlib.pyplot as plt
-plt.rcParams.update({'figure.figsize':(8,6), 'figure.dpi':150})
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import f1_score 
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
 from sklearn.svm import SVC, LinearSVC
+import sys
 import time
 from utils import document_term_matrix
+from utils import visualize
 
 
 #TODO:
@@ -31,9 +32,17 @@ logging.getLogger('').addHandler(console)
 def main():
 	st = time.time()
 
+	# ================
+	# corpus reading # 
+	# ================
+
+	corpus = pd.read_csv(args.path)
+
 	# =======================
 	# predefined parameters #
 	# =======================
+
+	n_jobs = args.n_jobs
 
 	if args.ngram:
 		ngram_range = tuple(args.ngram)
@@ -41,25 +50,33 @@ def main():
 		ngram_range = (1,1)
 
 	classruns = args.classruns
-	
-	# ================================
-	# corpus reading and vectorizing # 
-	# ================================
+	train_size = 0.8
+	test_size = 0.2
 
-	corpus = pd.read_csv(args.path)
+	# set dynamic cross validation value
+	cv = int((min(corpus.groupby('author')['text'].nunique())) * train_size)
+	if cv == 1:
+		cv += 1
+	elif cv > 10:
+		cv = 10
+	logging.info(f"Dynamically determined cross validation value is {cv}.")
 
-	vectorization_method = "Bag of words"
-	if args.zscore:
-		vectorization_method = "Z-Scores"
-		dtm, vector = document_term_matrix(corpus, "title", ngram_range=ngram_range, sparse=True, z_norm=True)
+	# =============
+	# vectorizing # 
+	# =============
+
+	if args.vectorization_method == "bow":
+		dtm, vector = document_term_matrix(corpus, "bow", "title", ngram_range=ngram_range, sparse=True)
+	elif args.vectorization_method == "zscore":
+		dtm, vector = document_term_matrix(corpus, "zscore", "title", ngram_range=ngram_range, sparse=True)
 		logging.info("Multinomial Naive Bayes can't be used with a z-score matrix. It won't be used.")
+	elif args.vectorization_method == "tfidf":
+		dtm, vector = document_term_matrix(corpus, "tfidf", "title", ngram_range=ngram_range, sparse=True)
+	elif args.vectorization_method == "cos":
+		dtm, vector = document_term_matrix(corpus, "cos", "title", ngram_range=ngram_range, sparse=True)
 	else:
-		if args.tfidf:
-			vectorization_method = "Tf-Idf"
-			dtm, vector = document_term_matrix(corpus, "title", ngram_range=ngram_range, tfidf=True, sparse=True, z_norm=False)
-		else:
-			dtm, vector = document_term_matrix(corpus, "title", ngram_range=ngram_range, sparse=True, z_norm=False)
-	
+		logging.info(f"The vectorization method '{args.vectorization_method}' isn't a available.")
+		sys.exit()
 
 	logging.info(f"Read and vectorized corpus ({int((time.time() - st)/60)} minute(s)).")
 
@@ -72,76 +89,248 @@ def main():
 	cv_dict = defaultdict(list)
 
 	for run in list(range(1, classruns+1)):
+
 		logging.info(f"Iteration: {run}/{classruns}")
 		X_train, X_test, y_train, y_test = train_test_split(dtm, 
 															corpus["author"],
-															test_size=0.2,
+															test_size=test_size,
 															stratify=corpus["author"],
 															shuffle=True)	
 		
+
 		# ==================================================
-		# K-Nearest Neighbors + z-scores (= Burrows Delta) #
+		# K-Nearest Neighbors (+ z-scores = Burrows Delta) #
 		# ==================================================
+
 		knn_clf = KNeighborsClassifier()
 		knn_model = knn_clf.fit(X_train, y_train)
 		knn_y_pred = knn_model.predict(X_test)
 		knn_f1_score = f1_score(y_test, knn_y_pred, average="micro")
-		knn_cross_val = np.mean(cross_val_score(knn_clf, X_train, y_train, cv=2, scoring="f1_micro"))
-		f1_dict["D+KNN"].append(knn_f1_score)
-		cv_dict["D+KNN"].append(knn_cross_val)
+		knn_cross_val = np.mean(cross_val_score(knn_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+		if args.vectorization_method == "zscore":
+			f1_dict["D-KNN"].append(knn_f1_score)
+			cv_dict["D-KNN"].append(knn_cross_val)
+		else:
+			f1_dict["KNN"].append(knn_f1_score)
+			cv_dict["KNN"].append(knn_cross_val)
+
+		# Hyperparameter optimization #
+
+		if args.use_tuning:
+			tknn_parameters = {"n_neighbors": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+							   "weights": ["uniform", "distance"],
+							   "algorithm": ["brute"],
+							   "leaf_size": [10, 20, 30, 40, 50],
+							   "p": [1, 2], 
+							   "n_jobs": [n_jobs]}
+							   
+			# weights: 'ball_tree' and 'kd_tree' doesn't work with sparse
+			# p = 1: manhatten, p = 2: euclidean (minkowski doesn't work)
+
+			tknn_grid = GridSearchCV(knn_clf, tknn_parameters, cv=cv, scoring="f1_micro")
+			tknn_grid.fit(X_train, y_train)
+
+			tknn_clf = KNeighborsClassifier(**tknn_grid.best_params_)
+			tknn_model = tknn_clf.fit(X_train, y_train)
+			tknn_y_pred = tknn_model.predict(X_test)
+			tknn_f1_score = f1_score(y_test, tknn_y_pred, average="micro")
+			tknn_cross_val = np.mean(cross_val_score(tknn_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+			if args.vectorization_method == "zscore":
+				f1_dict["D-tKNN"].append(knn_f1_score)
+				cv_dict["D-tKNN"].append(knn_cross_val)
+			else:
+				f1_dict["tKNN"].append(knn_f1_score)
+				cv_dict["tKNN"].append(knn_cross_val)
+
 
 		# =============================================================
-		# Nearest (shrunken) Centroids + z-scores (= Burrows Delta 2) #
+		# Nearest (shrunken) Centroids(+ z-scores = Burrows Delta 2) #
 		# =============================================================
+		
 		nsc_clf = NearestCentroid()
 		nsc_model = nsc_clf.fit(X_train, y_train)
 		nsc_y_pred = nsc_model.predict(X_test)
 		nsc_f1_score = f1_score(y_test, nsc_y_pred, average="micro")
-		nsc_cross_val = np.mean(cross_val_score(nsc_clf, X_train, y_train, cv=2, scoring="f1_micro"))
-		f1_dict["D+NSC"].append(nsc_f1_score)
-		cv_dict["D+NSC"].append(nsc_cross_val)
+		nsc_cross_val = np.mean(cross_val_score(nsc_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+		if args.vectorization_method == "zscore":
+			f1_dict["D-NSC"].append(nsc_f1_score)
+			cv_dict["D-NSC"].append(nsc_cross_val)
+		else:
+			f1_dict["NSC"].append(nsc_f1_score)
+			cv_dict["NSC"].append(nsc_cross_val)
 
-			
-		# ================================
-		# Logistic Regression + z-scores #
-		# ================================
-		"""
-		lr_clf = LogisticRegression(multi_class="multinomial", solver="saga")
-		lr_model = lr_clf.fit(X_train, y_train)
-		lr_y_pred = lr_model.predict(X_test)
-		lr_f1_score = f1_score(y_test, lr_y_pred, average="micro")
-		lr_cross_val = np.mean(cross_val_score(lr_clf, X_train, y_train, cv=2))
-		f1_dict["Logistic Regression"].append(lr_f1_score)
-		cv_dict["Logistic Regression"].append(lr_cross_val)
-		"""
+		# Hyperparameter optimization #
+		
+		if args.use_tuning:
+			tnsc_parameters = {"metric": ['euclidean', 'manhattan'],
+							   "shrink_threshold": [None]}
+
+			# metric: 'euclidean' and 'manhattan' only avaible metrics for sparse input
+			# shrink_threshold: is not supported for sparse input
+
+			tnsc_grid = GridSearchCV(nsc_clf, tnsc_parameters, cv=cv, scoring="f1_micro")
+			tnsc_grid.fit(X_train, y_train)
+
+			tnsc_clf = NearestCentroid(**tnsc_grid.best_params_)
+			tnsc_model = tnsc_clf.fit(X_train, y_train)
+			tnsc_y_pred = tnsc_model.predict(X_test)
+			tnsc_f1_score = f1_score(y_test, tnsc_y_pred, average="micro")
+			tnsc_cross_val = np.mean(cross_val_score(tnsc_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+			if args.vectorization_method == "zscore":
+				f1_dict["D-tNSC"].append(nsc_f1_score)
+				cv_dict["D-tNSC"].append(nsc_cross_val)
+			else:
+				f1_dict["tNSC"].append(nsc_f1_score)
+				cv_dict["tNSC"].append(nsc_cross_val)
+
+		
 		# =================================================
 		# Multinomial Naive Bayes (only without z-scores) #
 		# =================================================
 		
-		if not args.zscore:
+		if args.vectorization_method != "zscore":
 			mnb_clf = MultinomialNB()
 			mnb_model = mnb_clf.fit(X_train, y_train)
 			mnb_y_pred = mnb_model.predict(X_test)
 			mnb_f1_score = f1_score(y_test, mnb_y_pred, average="micro")
-			mnb_cross_val = np.mean(cross_val_score(mnb_clf, X_train, y_train, cv=2, scoring="f1_micro"))
+			mnb_cross_val = np.mean(cross_val_score(mnb_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
 			f1_dict["MNB"].append(mnb_f1_score)
 			cv_dict["MNB"].append(mnb_cross_val)
+		
+			# Hyperparameter optimization #
 
-		# ====================================
-		# Support Vector Machines + z-scores #
-		# ====================================
-		svm_clf = SVC(kernel="linear")
+			if args.use_tuning:
+				tmnb_parameters = {"alpha": [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05,
+											 0.1, 0.5, 1.0, 2.0, 3.0],
+								   "fit_prior": [True, False]}
+
+				tmnb_grid = GridSearchCV(mnb_clf, tmnb_parameters, cv=cv, scoring="f1_micro")
+				tmnb_grid.fit(X_train, y_train)
+
+				tmnb_clf = MultinomialNB(**tmnb_grid.best_params_)
+				tmnb_model = tmnb_clf.fit(X_train, y_train)
+				tmnb_y_pred = tmnb_model.predict(X_test)
+				tmnb_f1_score = f1_score(y_test, tmnb_y_pred, average="micro")
+				tmnb_cross_val = np.mean(cross_val_score(tmnb_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+				f1_dict["tMNB"].append(tmnb_f1_score)
+				cv_dict["tMNB"].append(tmnb_cross_val)
+
+		
+		
+		# ================================
+		# Linear Support Vector Machines #
+		# ================================
+		
+		lsvm_clf = LinearSVC()
+		lsvm_model = lsvm_clf.fit(X_train, y_train)
+		lsvm_y_pred = lsvm_model.predict(X_test)
+		lsvm_f1_score = f1_score(y_test, lsvm_y_pred, average="micro")
+		lsvm_cross_val = np.mean(cross_val_score(lsvm_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+		f1_dict["LSVM"].append(lsvm_f1_score)
+		cv_dict["LSVM"].append(lsvm_cross_val)
+		
+
+		# Hyperparameter optimization #
+		
+		if args.use_tuning:
+			tlsvm_parameters = {"penalty": ["l2"],
+							   "loss": ["squared_hinge"],
+							   "C": [0.0001, 0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0],
+							   "max_iter": [1000, 1125, 1200, 1500, 2000]}
+			
+			# penalty = "l1" doesn't work with "squared_hinge" so it won't be used
+			# loss = "hinge" doesn't work with "l2" so it won't be used
+			tlsvm_grid = GridSearchCV(lsvm_clf, tlsvm_parameters, cv=cv, scoring="f1_micro")
+			tlsvm_grid.fit(X_train, y_train)
+
+			tlsvm_clf = LinearSVC(**tlsvm_grid.best_params_)
+			tlsvm_model = tlsvm_clf.fit(X_train, y_train)
+			tlsvm_y_pred = tlsvm_model.predict(X_test)
+			tlsvm_f1_score = f1_score(y_test, tlsvm_y_pred, average="micro")
+			tlsvm_cross_val = np.mean(cross_val_score(tlsvm_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+			f1_dict["tLSVM"].append(tlsvm_f1_score)
+			cv_dict["tLSVM"].append(tlsvm_cross_val)
+		
+		# =====================
+		# Logistic Regression #
+		# =====================
+		
+		# every solver except "liblinear" had problems with the convergence
+		lr_clf = LogisticRegression(multi_class="ovr", solver="liblinear")
+		lr_model = lr_clf.fit(X_train, y_train)
+		lr_y_pred = lr_model.predict(X_test)
+		lr_f1_score = f1_score(y_test, lr_y_pred, average="micro")
+		lr_cross_val = np.mean(cross_val_score(lr_clf, X_train, y_train, cv=cv))
+		f1_dict["LR"].append(lr_f1_score)
+		cv_dict["LR"].append(lr_cross_val)
+
+		# Hyperparameter optimization #
+		
+		if args.use_tuning:
+			tlr_parameters = {"penalty": ["l1"],
+							  "tol": [0.0001, 0.001, 0.01, 0.1],
+							  "C": [0.0001, 0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0],
+							  "solver": ["liblinear"],
+							  "max_iter": [1000, 2000, 3000, 4000, 5000, 6000],
+							  "multi_class": ["ovr"],
+							  "n_jobs": [n_jobs]}
+
+			# solver only "saga" because its good for large datasets and it supports elasticnet-penalty
+			tlr_grid = GridSearchCV(lr_clf, tlr_parameters, cv=cv, scoring="f1_micro")
+			tlr_grid.fit(X_train, y_train)
+
+			tlr_clf = LogisticRegression(**tlr_grid.best_params_)
+			tlr_model = tlr_clf.fit(X_train, y_train)
+			tlr_y_pred = tlr_model.predict(X_test)
+			tlr_f1_score = f1_score(y_test, tlr_y_pred, average="micro")
+			tlr_cross_val = np.mean(cross_val_score(tlr_clf, X_train, y_train, cv=cv))
+			f1_dict["tLR"].append(tlr_f1_score)
+			cv_dict["tLR"].append(tlr_cross_val)
+		
+		"""
+		# ==========================================
+		# Support Vector Machines (without linear) #
+		# ==========================================
+		
+		svm_clf = SVC()
 		svm_model = svm_clf.fit(X_train, y_train)
 		svm_y_pred = svm_model.predict(X_test)
 		svm_f1_score = f1_score(y_test, svm_y_pred, average="micro")
-		svm_cross_val = np.mean(cross_val_score(svm_clf, X_train, y_train, cv=2, scoring="f1_micro"))
+		svm_cross_val = np.mean(cross_val_score(svm_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
 		f1_dict["SVM"].append(svm_f1_score)
 		cv_dict["SVM"].append(svm_cross_val)
+		
+
+		# Hyperparameter optimization #
+		
+		if args.use_tuning:
+			tsvm_parameters = {"C": [0.0001, 0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0],
+							   "kernel": ["poly"],#, "rbf", "sigmoid"],
+							   "degree": [1, 2, 3, 4, 5],
+							   "shrinking": [True, False],
+							   "tol": [0.0001, 0.001, 0.01, 0.1]}
+			# kernel = "precomputed" doesn't work
+
+			tsvm_grid = GridSearchCV(svm_clf, tsvm_parameters, cv=cv, scoring="f1_micro")
+			tsvm_grid.fit(X_train, y_train)
+
+			print(tsvm_grid.best_params_)
+
+			tsvm_clf = SVC(**tsvm_grid.best_params_)
+			tsvm_model = tsvm_clf.fit(X_train, y_train)
+			tsvm_y_pred = tsvm_model.predict(X_test)
+			tsvm_f1_score = f1_score(y_test, tsvm_y_pred, average="micro")
+			tsvm_cross_val = np.mean(cross_val_score(tsvm_clf, X_train, y_train, cv=cv, scoring="f1_micro"))
+			f1_dict["tSVM"].append(tsvm_f1_score)
+			cv_dict["tSVM"].append(tsvm_cross_val)
+		
+		"""
 
 	# ================
 	# Saving results #
 	# ================
-	
+
+
 	final_f1_dict = {}
 	for (f1_k, f1_v), (cv_k, cv_v) in zip(f1_dict.items(), cv_dict.items()):
 		if f1_k == cv_k:
@@ -150,26 +339,23 @@ def main():
 	results = pd.DataFrame(final_f1_dict).round(3).T
 
 	
+	
 	if args.save_date:
-		csv_name = f"classification_{args.output_name} ({vectorization_method}) ({datetime.now():%d.%m.%y}_{datetime.now():%H:%M})"
+		csv_name = f"classification_{args.corpus_name}({args.vectorization_method}_{classruns}_{args.ngram})_({datetime.now():%d.%m.%y}_{datetime.now():%H:%M})"
 	else:
-		csv_name = f"classification_{args.output_name} ({vectorization_method})"
+		csv_name = f"classification_{args.corpus_name}({args.vectorization_method}_{classruns}_{args.ngram})"
 	results.to_csv(f"../data/tables/{csv_name}.csv")
 
+	if args.visualization:
+		logging.info(r"Saving figure of results to data/figures/results/")
+		visualize(results,
+			  	  "bar_horizontal",
+			  	  classruns,
+			  	  ngram=args.ngram, 
+			  	  vectorization_method=args.vectorization_method)
+
+
 	
-	ax = results.plot.bar(color=["#003f5c","#a05195"], edgecolor="black")
-	for p in ax.patches: 
-	    ax.annotate(np.round(p.get_height(),decimals=3), (p.get_x()+p.get_width()/2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
-	plt.yticks(np.arange(0,1.1,0.1))
-	plt.title(f" Corpus: {args.output_name}\n Weighting: {vectorization_method} \n N-grams: {args.ngram} \n Train-test iterations: {classruns} ", loc="left")
-
-
-	if args.save_date:
-		figure_name = f"results_bar_{args.output_name} ({vectorization_method}) ({datetime.now():%d.%m.%y}_{datetime.now():%H:%M})"
-	else:
-		figure_name = f"results_bar_{args.output_name} ({vectorization_method})"
-	plt.savefig(f'../data/figures/results/{figure_name}.png', dpi=300, bbox_inches='tight')
-
 	logging.info(f"Run-time: {int((time.time() - st)/60)} minute(s).")
 
 if __name__ == "__main__":
@@ -177,11 +363,13 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog="classification", description="Comparison of different classifier.")
 	parser.add_argument("path", type=str, help="Path to the corpus as csv-file.")
 	parser.add_argument("--classruns", "-cr", type=int, nargs="?", default=20, help="Sets the number of classification runs.")
-	parser.add_argument("--ngram", "-ng", type=int, nargs="?", default=(1, 1), help="Passes the ngram range.")
-	parser.add_argument("--output_name", "-on", type=str, nargs="?", default="corpus", help="Indicates the name of the corpus for the output file.")
+	parser.add_argument("--corpus_name", "-cn", type=str, nargs="?", default="prose", help="Indicates the name of the corpus for the output file.")
+	parser.add_argument("--ngram", "-ng", type=int, nargs="?", default=(1,1), help="Passes the ngram range.")
+	parser.add_argument("--n_jobs", "-nj", type=int, default=1, help="Indicates the number of processors used for computation.")
 	parser.add_argument("--save_date", "-sd", action="store_true", help="Indicates if the creation date of the results should be saved.")
-	parser.add_argument("--tfidf", "-tf", action="store_true", help="Indicates if tfidf should be used for data vectorization.")
-	parser.add_argument("--zscore", "-zs", action="store_true", help="Indicates if z-scores normalization should be used.")
+	parser.add_argument("--use_tuning", "-ut", action="store_true", help="Indicates if hyperparameter optimization should be used.")
+	parser.add_argument("--vectorization_method", "-vm", type=str, default="bow", help="Indicates the vectorization method. Default is 'bow'. Other possible values are 'zscore', 'tfidf' and 'cos'.")
+	parser.add_argument("--visualization", "-v", action="store_true", help="Indicates if results should be visualized.")
 
 	
 	args = parser.parse_args()
